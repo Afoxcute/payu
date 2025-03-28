b9dc3e3080f04792f052dbdf0b129de5233d11bf
Afoxcute <jwavolabilove16@gmail.com>
2025-03-28 20:14:52 +0000

    chat fixes

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -193,216 +193,232 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest) {
-  const update = (await req.json()) as TelegramBot.Update;
-  console.log("Received update:", update);
-  if (update.message?.contact) {
-    await db.user.upsert({
-      where: { phone: update.message.contact.phone_number },
-      create: {
-        telegramId: String(update.message.contact.user_id),
-        phone: update.message.contact.phone_number,
-      },
-      update: {
-        telegramId: String(update.message.chat.id),
-      },
-    });
-    console.log("Received contact:", update.message.contact);
-    await bot.sendMessage(
-      update.message.chat.id,
-      "Thank you! We've registered your phone number. You can now send and receive money. All within Telegram!",
-    );
-  }
-  // Pass updates to the bot
-  if (update.inline_query) {
-    // @ts-expect-error - Ignore TypeScript error for inline_query event
-    bot.emit("inline_query", update.inline_query);
-  } else if (update.callback_query) {
-    // @ts-expect-error - Ignore TypeScript error for inline_query event
-    bot.emit("callback_query", update.callback_query);
-  } else if (update.message) {
-    console.log("received:", update.message.text);
-    if (update.message.text === "/start") {
-      const chatId = update.message.chat.id;
-      const userId = update.message.from?.id;
-
-      // Quick reply just to not leave them hanging
-
-      if (userId) {
-        const user = await db.user.findFirst({
-          where: { telegramId: String(userId) },
+  try {
+    const update = (await req.json()) as TelegramBot.Update;
+    console.log("Received update:", update);
+    
+    // Process contact information
+    if (update.message?.contact) {
+      try {
+        await db.user.upsert({
+          where: { phone: update.message.contact.phone_number },
+          create: {
+            telegramId: String(update.message.contact.user_id),
+            phone: update.message.contact.phone_number,
+          },
+          update: {
+            telegramId: String(update.message.chat.id),
+          },
         });
-        if (!user?.phone) {
-          await bot.sendMessage(
-            chatId,
-            "Let's get you started. Please share your phone number with us. We'll use it to search for pending transactions associated with your account.",
-            {
-              reply_markup: {
-                keyboard: [
-                  [
-                    {
-                      text: "Share your phone number",
-                      request_contact: true, // Requests the phone number
-                    },
-                  ],
-                ],
-                resize_keyboard: true,
-                one_time_keyboard: true,
-              },
-            },
-          );
-          return NextResponse.json({ status: "ok" });
-        } else {
-          await bot.sendMessage(
-            chatId,
-            "Welcome back! You can use the /send command to send money or /redeem to review and redeem pending transactions.",
-          );
-          return NextResponse.json({ status: "ok" });
-        }
-      }
-    } else if (update.message.text === "/redeem") {
-      const chatId = update.message.chat.id;
-      const userId = update.message.from?.id;
-      if (userId) {
-        const user = await db.user.findFirst({
-          where: { telegramId: String(userId) },
-        });
-        if (!user?.phone) {
-          await bot.sendMessage(
-            chatId,
-            "Let's get you started. Please share your phone number with us. We'll use it to search for pending transactions associated with your account.",
-            {
-              reply_markup: {
-                keyboard: [
-                  [
-                    {
-                      text: "Share your phone number",
-                      request_contact: true, // Requests the phone number
-                    },
-                  ],
-                ],
-                resize_keyboard: true,
-                one_time_keyboard: true,
-              },
-            },
-          );
-          return NextResponse.json({ status: "ok" });
-        }
-
-        // Step 1: Prompt the user to enter the collection code
-        const transfers = await db.transfer.findMany({
-          where: { recipientPhone: user.phone, status: TransferStatus.PENDING },
-          orderBy: { createdAt: "asc" },
-          take: 5,
-          include: { sender: true },
-        });
-        console.log("transfers:", transfers);
-        if (!transfers.length) {
-          await bot.sendMessage(
-            chatId,
-            "There are no pending transactions associated with your account.",
-          );
-          return NextResponse.json({ status: "ok" });
-        }
-
+        console.log("Received contact:", update.message.contact);
         await bot.sendMessage(
-          chatId,
-          "You have pending transactions associated with your account. Please select one to redeem.",
-          {
-            reply_markup: {
-              inline_keyboard: transfers.map((transfer) => [
+          update.message.chat.id,
+          "Thank you! We've registered your phone number. You can now send and receive money. All within Telegram!",
+        );
+      } catch (error) {
+        console.error("Error processing contact:", error);
+      }
+    }
+    
+    // Pass updates to the bot
+    if (update.inline_query) {
+      // @ts-expect-error - Ignore TypeScript error for inline_query event
+      bot.emit("inline_query", update.inline_query);
+    } else if (update.callback_query) {
+      // @ts-expect-error - Ignore TypeScript error for inline_query event
+      bot.emit("callback_query", update.callback_query);
+    } else if (update.message) {
+      console.log("received:", update.message.text);
+      
+      // Handle /start command
+      if (update.message.text === "/start") {
+        const chatId = update.message.chat.id;
+        const userId = update.message.from?.id;
+
+        // Quick reply just to not leave them hanging
+        try {
+          await bot.sendMessage(chatId, "Processing your request...");
+          
+          if (userId) {
+            const user = await db.user.findFirst({
+              where: { telegramId: String(userId) },
+            });
+            if (!user?.phone) {
+              await bot.sendMessage(
+                chatId,
+                "Let's get you started. Please share your phone number with us. We'll use it to search for pending transactions associated with your account.",
                 {
-                  text: `Redeem $${Number(transfer.amount).toLocaleString(
-                    "en-US",
-                    {
-                      minimumFractionDigits: 2,
-                      maximumFractionDigits: 2,
-                    },
-                  )} from ${transfer.sender?.firstName ?? transfer.sender?.telegramUsername ?? transfer.sender?.phone ?? "Unknown"} sent ${dayjs(transfer.createdAt).fromNow()}`,
-                  callback_data: `redeem_${transfer.id}`,
+                  reply_markup: {
+                    keyboard: [
+                      [
+                        {
+                          text: "Share your phone number",
+                          request_contact: true, // Requests the phone number
+                        },
+                      ],
+                    ],
+                    resize_keyboard: true,
+                    one_time_keyboard: true,
+                  },
                 },
-              ]),
-            },
-          },
-        );
-      }
-    } // Directly check if the message text is "/send"
-    else if (update.message.text === "/send") {
-      const chatId = update.message.chat.id;
-      let userPhone = "";
-      const userId = update.message.from?.id;
-      if (userId) {
-        db.user
-          .findFirst({
-            where: { telegramId: String(userId) },
-          })
-          .then((user) => {
-            if (user?.phone) {
-              userPhone = user.phone;
+              );
+            } else {
+              await bot.sendMessage(
+                chatId,
+                "Welcome back! You can use the /send command to send money or /redeem to review and redeem pending transactions.",
+              );
             }
-          })
-          .catch(console.error);
+          }
+        } catch (error) {
+          console.error("Error handling /start command:", error);
+        }
       }
+      
+      // Rest of your message handling code...
+      if (update.message.text === "/redeem") {
+        const chatId = update.message.chat.id;
+        const userId = update.message.from?.id;
+        if (userId) {
+          const user = await db.user.findFirst({
+            where: { telegramId: String(userId) },
+          });
+          if (!user?.phone) {
+            await bot.sendMessage(
+              chatId,
+              "Let's get you started. Please share your phone number with us. We'll use it to search for pending transactions associated with your account.",
+              {
+                reply_markup: {
+                  keyboard: [
+                    [
+                      {
+                        text: "Share your phone number",
+                        request_contact: true, // Requests the phone number
+                      },
+                    ],
+                  ],
+                  resize_keyboard: true,
+                  one_time_keyboard: true,
+                },
+              },
+            );
+            return NextResponse.json({ status: "ok" });
+          }
 
-      // Step 0: Prompt the user to enter the recipient's full name
-      await bot.sendMessage(chatId, promptForRecipientName(), {
-        parse_mode: "Markdown",
-      });
+          // Step 1: Prompt the user to enter the collection code
+          const transfers = await db.transfer.findMany({
+            where: { recipientPhone: user.phone, status: TransferStatus.PENDING },
+            orderBy: { createdAt: "asc" },
+            take: 5,
+            include: { sender: true },
+          });
+          console.log("transfers:", transfers);
+          if (!transfers.length) {
+            await bot.sendMessage(
+              chatId,
+              "There are no pending transactions associated with your account.",
+            );
+            return NextResponse.json({ status: "ok" });
+          }
 
-      // Step 1: Prompot for recipient's phone number
-      bot.once("message", async (nameMsg) => {
-        const recipientName = nameMsg.text;
-
-        await bot.sendMessage(
-          chatId,
-          promptForPhoneNumber(String(recipientName)),
-          {
-            parse_mode: "Markdown",
-          },
-        );
-
-        // Step 2: Capture the recipient's name and ask for the country
-        bot.once("message", async (phoneMsg) => {
-          console.log("phoneMsg:", phoneMsg);
-          const phoneNumber = parsePhoneNumber(String(phoneMsg.text));
-          console.log("phoneNumber:", phoneNumber);
-
-          // Ask for the country
           await bot.sendMessage(
             chatId,
-            promptForCountry(String(recipientName)),
+            "You have pending transactions associated with your account. Please select one to redeem.",
+            {
+              reply_markup: {
+                inline_keyboard: transfers.map((transfer) => [
+                  {
+                    text: `Redeem $${Number(transfer.amount).toLocaleString(
+                      "en-US",
+                      {
+                        minimumFractionDigits: 2,
+                        maximumFractionDigits: 2,
+                      },
+                    )} from ${transfer.sender?.firstName ?? transfer.sender?.telegramUsername ?? transfer.sender?.phone ?? "Unknown"} sent ${dayjs(transfer.createdAt).fromNow()}`,
+                    callback_data: `redeem_${transfer.id}`,
+                  },
+                ]),
+              },
+            },
+          );
+        }
+      } // Directly check if the message text is "/send"
+      else if (update.message.text === "/send") {
+        const chatId = update.message.chat.id;
+        let userPhone = "";
+        const userId = update.message.from?.id;
+        if (userId) {
+          db.user
+            .findFirst({
+              where: { telegramId: String(userId) },
+            })
+            .then((user) => {
+              if (user?.phone) {
+                userPhone = user.phone;
+              }
+            })
+            .catch(console.error);
+        }
+
+        // Step 0: Prompt the user to enter the recipient's full name
+        await bot.sendMessage(chatId, promptForRecipientName(), {
+          parse_mode: "Markdown",
+        });
+
+        // Step 1: Prompot for recipient's phone number
+        bot.once("message", async (nameMsg) => {
+          const recipientName = nameMsg.text;
+
+          await bot.sendMessage(
+            chatId,
+            promptForPhoneNumber(String(recipientName)),
             {
               parse_mode: "Markdown",
             },
           );
 
-          // Step 3: Capture the country and then ask for the amount
-          bot.once("message", async (countryMsg) => {
-            const recipientCountry = countryMsg.text;
+          // Step 2: Capture the recipient's name and ask for the country
+          bot.once("message", async (phoneMsg) => {
+            console.log("phoneMsg:", phoneMsg);
+            const phoneNumber = parsePhoneNumber(String(phoneMsg.text));
+            console.log("phoneNumber:", phoneNumber);
 
-            // Ask for the amount to send
+            // Ask for the country
             await bot.sendMessage(
               chatId,
-              promptForAmount(String(recipientName), String(recipientCountry)),
+              promptForCountry(String(recipientName)),
               {
                 parse_mode: "Markdown",
               },
             );
 
-            // Step 4: Capture the amount
-            bot.once("message", async (amountMsg) => {
-              const amount = parseAmount(amountMsg.text ?? "1");
-              const selectedCountry = countries.find(
-                (c) => c.value === mapCountry(recipientCountry),
-              );
-              const rateTo =
-                countryCurrencyMap[
-                  selectedCountry?.value as keyof typeof countryCurrencyMap
-                ] ?? "MXN";
-              const rate = await getRate("USD", rateTo);
-              // Confirm the transaction details with the user
+            // Step 3: Capture the country and then ask for the amount
+            bot.once("message", async (countryMsg) => {
+              const recipientCountry = countryMsg.text;
+
+              // Ask for the amount to send
               await bot.sendMessage(
                 chatId,
-                `📝 *Transaction Summary*
+                promptForAmount(String(recipientName), String(recipientCountry)),
+                {
+                  parse_mode: "Markdown",
+                },
+              );
+
+              // Step 4: Capture the amount
+              bot.once("message", async (amountMsg) => {
+                const amount = parseAmount(amountMsg.text ?? "1");
+                const selectedCountry = countries.find(
+                  (c) => c.value === mapCountry(recipientCountry),
+                );
+                const rateTo =
+                  countryCurrencyMap[
+                    selectedCountry?.value as keyof typeof countryCurrencyMap
+                  ] ?? "MXN";
+                const rate = await getRate("USD", rateTo);
+                // Confirm the transaction details with the user
+                await bot.sendMessage(
+                  chatId,
+                  `📝 *Transaction Summary*
 
 *Recipient Information*
 - *Name*: ${recipientName}
@@ -412,43 +428,59 @@ export async function POST(req: NextRequest) {
 *Transaction Details*
 - *Amount*: ${amount} USD
 - *${recipientName}* will receive $${(
-                  Number(rate) * Number(amount)
-                ).toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })} ${rateTo}
+                    Number(rate) * Number(amount)
+                  ).toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })} ${rateTo}
 
 *Exchange Rate*: $1.00 USD = ${rate} ${rateTo}
 
 ⬇️ *Action Required*
 If everything looks correct, tap *Review & Confirm* to proceed. We'll guide you through the rest of the process.
 `,
-                {
-                  parse_mode: "Markdown",
-                  reply_markup: {
-                    inline_keyboard: [
-                      [
-                        {
-                          text: "Review & Confirm",
-                          web_app: {
-                            url: `${env.NEXT_PUBLIC_APP_URL}/transfer-preview?recipient=${recipientName}&country=${recipientCountry?.toLowerCase()}&amount=${amount}&recipientPhone=${phoneNumber?.replace("+", "")}&senderPhone=${userPhone?.replace("+", "")}`,
+                  {
+                    parse_mode: "Markdown",
+                    reply_markup: {
+                      inline_keyboard: [
+                        [
+                          {
+                            text: "Review & Confirm",
+                            web_app: {
+                              url: `${env.NEXT_PUBLIC_APP_URL}/transfer-preview?recipient=${recipientName}&country=${recipientCountry?.toLowerCase()}&amount=${amount}&recipientPhone=${phoneNumber?.replace("+", "")}&senderPhone=${userPhone?.replace("+", "")}`,
+                            },
                           },
-                        },
-                        { text: "❌ Cancel", callback_data: "cancel" },
+                          { text: "❌ Cancel", callback_data: "cancel" },
+                        ],
                       ],
-                    ],
+                    },
                   },
-                },
-              );
+                );
+              });
             });
           });
         });
-      });
-    } else {
-      // @ts-expect-error - Ignore TypeScript error for message event
-      bot.emit("message", update.message);
+      } else {
+        // @ts-expect-error - Ignore TypeScript error for message event
+        bot.emit("message", update.message);
+      }
     }
+    
+    // Always return a 200 OK response to Telegram
+    return new NextResponse(JSON.stringify({ status: "ok" }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    // Still return 200 OK to Telegram to prevent repeated failed delivery attempts
+    return new NextResponse(JSON.stringify({ status: "error", message: "Internal server error" }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
-
-  return NextResponse.json({ status: "ok" });
 }
