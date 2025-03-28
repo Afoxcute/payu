33a9f4f322cea686106e60fe26e4302e5bbe0ec8
Afoxcute <jwavolabilove16@gmail.com>
2025-03-28 18:17:30 +0000

    fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,7 +18,6 @@ const config = {
     "@typescript-eslint/consistent-type-imports": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
